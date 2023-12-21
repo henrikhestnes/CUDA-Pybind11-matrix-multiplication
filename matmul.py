e2e6d0723fb5435099f373a5d4b4ad35f1f768af
henrikhestnes <hahestne@stud.ntnu.no>
2023-12-21 14:16:33 +0100

    add Python native implementation to compare runtime

--- matmul.py
+++ matmul.py
@@ -1,16 +1,15 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 
 import sys
 sys.path.append('build')
 import gpu_library
 
-
+np.random.seed(0)
 M, N, K = 32*5, 32*5, 32*5
+
 A = np.random.rand(M, N)
 B = np.random.rand(N, K)
-
 
 start = time.perf_counter()
 C_CPU = np.zeros(M*K)
@@ -36,9 +35,20 @@ print("GPU with shared memory time: " + str(end-start))
 start = time.perf_counter()
 C_PYTHON = A @ B
 end = time.perf_counter()
-print("Python time: " + str(end-start))
+print("Python optmimized time: " + str(end-start))
+
+
+start = time.perf_counter()
+C_PYTHON_NATIVE = [[0 for _ in range(K)] for _ in range(M)]
+for i in range(M):
+    for j in range(K):
+        for k in range(N):
+            C_PYTHON_NATIVE[i][j] += A[i][k] * B[k][j]
+end = time.perf_counter()
+print("Python native time: " + str(end-start))
 
 
 print(f"\nCPU in C++ result correct: {np.allclose(C_CPU.reshape(M, K),C_PYTHON)}")
 print(f"GPU with only global memory correct: {np.allclose(C_GPU_GLOBAL.reshape(M, K),C_PYTHON)}")
 print(f"GPU with shared memory correct: {np.allclose(C_GPU_SHARED.reshape(M, K),C_PYTHON)}")
+print(f"Python native correct: {np.allclose(np.array(C_PYTHON_NATIVE).reshape(M, K),C_PYTHON)}")
